2a7a5f5dac024df18b8baf2cced7c85f7720c5df
Peter J. Gibbons <135149809+Diggleda@users.noreply.github.com>
2025-09-18 22:13:15 -0400

    Finished content.js

---
+++ content.js
@@ -0,0 +1,61 @@
+window.PRD_CONTENT = {
+  hero: {
+    label: "Personal Portfolio | Updated Sep 18, 2025",
+    title: "Peter J. Gibbons",
+    image: "photos/PeterGibbons.png",
+    description:
+      "I am devoted to non-duality and love. I am the fifth generation of the Walsh Construction family, an electrical engineer from the University of Notre Dame, and passionate about entrepreneurship. My greatest weakness is self-sabatoging and limiting my connection to Source, which I am quickly realizing never was severed.",
+  },
+  projects: [
+    {
+      date: "Summer 2024",
+      title: "JLG",
+      description:
+        "Developed the desktop capability to digitally simulate 24/48V battery packs and have begun to demonstrate the value of such a system in reducing the cost and time requirements of battery management system testing and verification.",
+      image: "photos/JLG.png",
+      links: [
+        {
+          label: "JLG Website",
+          href: "https://www.jlg.com/en",
+        },
+      ],
+    },
+    {
+      date: "School 2024-2025",
+      title: "Power Control Board",
+      description:
+        "Cubesat Electrical Systems Architect and Designer at IrishSat. Responsible for 20 electrical, computer and software engineers taking on the journey of development. Developed this board over a dozen iterations. It is the heart of the electrical system of our cubesat responsible for solar energy storage and distribution.",
+      image: "photos/PCB.png",
+      download: "files/PCB.zip",
+      downloadLabel: "Download PCB Files",
+      links: [
+        {
+          label: "IrishSat Website",
+          href: "https://ndirishsat.com",
+          target: "_blank",
+        },
+      ],
+    },
+    {
+      date: "2025-NOW",
+      title: "Drapery Reveal™",
+      description:
+        "Drapery Reveal™ is a site I am making available for everyone to use. Back in undergrad, I had a 3D model I made for a class project. I also had to make a presentation for this project, and wanted a good looking way to unveil the model. I figured out I would have to learn blender, and therefore, everyone else would too for something that really should be easier. It has evolved into what you see here today, a customizable scene-based unveiling animation. You can use your own textures, colors, and environment settings!",
+      image: "photos/DraperyReveal.png",
+      links: [
+        {
+          label: "www.draperyreveal.com",
+          href: "https://www.draperyreveal.com",
+          target: "_blank",
+        },
+      ],
+    }
+  ],
+  contact: {
+    title: "Contact",
+    body: "Ready to connect or collaborate? Reach out to continue the conversation and explore opportunities.",
+    buttonText: "Email",
+    href: "mailto:petergibbons7@icloud.com",
+  },
+  footerNote: "The world can only see us as we see ourselves.",
+};
